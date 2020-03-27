298c5b6e2926d0cc7ce64accd14125809c4926e8
rocomenty <cheng.luo@wustl.edu>
2020-03-26 21:11:27 -0700

    django backend setup

---
+++ cheng/jingyi_site/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class JingyiSiteConfig(AppConfig):
+    name = 'jingyi_site'
---
+++ chengenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/chengluo/Documents/GitHub/cheng/chengenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
